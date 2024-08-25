da04fa7d37f50af8664279f174e0d972d745a75f
RajaSrujana27 <adapasrujana27@gmail.com>
2024-08-25 11:36:45 +0530

    tasks

--- to-do list/script.js
+++ to-do list/script.js
@@ -140,6 +140,44 @@ function showModal(task) {
         <p>Completed: ${task.completed ? 'Yes' : 'No'}</p>
     `;
 }
+function setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    const cname = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cname) == 0) {
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
+
+function deleteCookie(name) {
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+// Save theme preference to cookie
+function saveThemePreference(theme) {
+    setCookie('preferredTheme', theme, 30); // Save for 30 days
+}
+
+// Retrieve and apply theme preference from cookie
+function applySavedTheme() {
+    const theme = getCookie('preferredTheme');
+    if (theme) {
+        document.body.classList.add(theme);
+    }
+}
 
 
 
